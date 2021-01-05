2fafae80edd0f6ebbf384348447fb38f8769e7d3
Alissa Lam <shiky5678@gmail.com>
2021-01-06 01:18:07 +0800

    update1

--- w.py
+++ w.py
@@ -1,12 +1,14 @@
 password = 'a123'
 x = 3 # 輸入機會次數
-while True:
+while x > 0:
+	x = x - 1
 	pwd = input('please enter your password: ')
 	if pwd == password:
 		print('成功登入!')
 		break
 	else:
-		x = x - 1
-		print('密碼錯誤, 還有', x, '次機會')
-		if x == 0:
-			break
+		print('密碼錯誤')
+		if x > 0:
+			print('還有' , x, '次機會')
+		else:
+			print('冇機會啦! 鎖帳了')
