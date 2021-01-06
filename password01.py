3b5e454362a910ff1e7e5e45237f5bad7204ba16
Alissa Lam <shiky5678@gmail.com>
2021-01-06 18:10:30 +0800

    update2

---
+++ password01.py
@@ -0,0 +1,17 @@
+password = 'a123456'
+x = 5
+while x > 0:
+	x -= 1
+	pwd = input('請輸入你的密碼: ')
+	if pwd == password :
+		print('登入成功!')
+		break
+	else :
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x, '次機會')
+		
+		if x == 1:
+			print('最後一次機會喔')
+		
+print('偷帳的?')
